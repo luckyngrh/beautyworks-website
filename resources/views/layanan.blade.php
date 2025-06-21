b569bbd2d4baf04040cc90e98f859ae28f24a190
luckyngrh <nuglucky03@gmail.com>
2025-06-21 10:36:22 +0700

    feat: update service images and improve consultation link on services page

--- resources/views/layanan.blade.php
+++ resources/views/layanan.blade.php
@@ -5,7 +5,7 @@
       <div class="flex items-center justify-center min-h-14 mb-5">
         <h3 class="text-pink-600 text-xl font-semibold">Make-Up Wedding</h3>
       </div>
-      <img src="{{ asset('images/fifi.jpg') }}" alt="Makeup Wedding"
+      <img src="{{ asset('images/wedding4.jpg') }}" alt="Makeup Wedding"
         class="mx-auto object-cover rounded-lg shadow-md mb-2">
       <p class="text-gray-700 min-h-20 mb-2">Untuk Akad & Resepsi, bisa Retouch</p>
       <a href="{{ route('reservasi') }}"
@@ -17,9 +17,9 @@
     <!-- Reguler Make-up -->
     <div>
       <div class="flex items-center justify-center min-h-14 mb-5">
-        <h3 class="text-pink-600 text-xl font-semibold">Reguler Make-up</h3>
+        <h3 class="text-pink-600 text-xl font-semibold">Make-up Reguler</h3>
       </div>
-      <img src="{{ asset('images/fifi.jpg') }}" alt="Makeup Wedding"
+      <img src="{{ asset('images/events2.jpg') }}" alt="Makeup Wedding"
         class="mx-auto object-cover rounded-lg shadow-md mb-2">
       <p class="text-gray-700 min-h-20 mb-2">Make-Up Photoshoot, Make-Up Wisuda, Make-Up Party</p>
       <a href="{{ route('reservasi') }}"
@@ -33,7 +33,7 @@
       <div class="flex items-center justify-center min-h-14 mb-5">
         <h3 class="text-pink-600 text-xl font-semibold">Make-Up Class</h3>
       </div>
-      <img src="{{ asset('images/fifi.jpg') }}" alt="Makeup Wedding"
+      <img src="{{ asset('images/class5.jpg') }}" alt="Makeup Wedding"
         class="mx-auto object-cover rounded-lg shadow-md mb-2">
       <p class="text-gray-700 min-h-20 mb-2">Basic Makeup Class, Professional Class, Private Class</p>
       <a href="{{ route('reservasi') }}"
@@ -47,20 +47,14 @@
       <div class="flex items-center justify-center min-h-14 mb-5">
         <h3 class="text-pink-600 text-xl font-semibold">Make-up Consultation</h3>
       </div>
-      <img src="{{ asset('images/fifi.jpg') }}" alt="Makeup Wedding"
+      <img src="{{ asset('images/class1.jpg') }}" alt="Makeup Wedding"
         class="mx-auto object-cover rounded-lg shadow-md mb-2">
       <p class="text-gray-700 min-h-20 mb-2">Consultations are available via WhatsApp, Instagram, or live chat.</p>
-      <a href="{{ route('reservasi') }}"
+      <a href=""
         class="inline-block px-4 py-2 border border-pink-500 text-pink-500 rounded hover:bg-pink-50 transition">
-        Reservation Now
+        Chat Now
       </a>
     </div>
   </div>
-  <!-- <div class="flex flex-col items-center gap-4 mt-6">
-    <h2 class="font-bold text-2xl text-center">Wedding Make-Up</h2>
-    <img class="w-[10px]" src="{{ asset('images/fifi.jpg') }}" alt="Makeup Wedding">
-    <p class="">Untuk Akad & Resepsi, bisa Retouch</p>
-    <a class="btn btn-primary btn-outline" href="">Reservasi Sekarang</a>
-  </div> -->
 
 </x-layout>
