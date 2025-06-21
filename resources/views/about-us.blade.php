e8e6af190ac2128a2dbf962bb610f30b352c6792
luckyngrh <nuglucky03@gmail.com>
2025-06-21 13:27:19 +0700

    feat: improve 'About Us' page layout with enhanced spacing and typography

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -3,9 +3,9 @@
     <div class="flex flex-[20%] justify-center items-center">
       <img src="{{ asset('images/aboutus.png') }}" alt="owner picture" class="rounded-full object-cover">
     </div>
-    <div class="flex-[80%] justify-center items-center pt-6">
+    <div class="flex-[80%] justify-center items-center pt-10">
       <h2 class="font-semibold text-center text-2xl mb-2">About Us</h1>
-      <p class="text-justify pl-9 pr-5">
+      <p class="text-justify pl-9 pr-5 text-lg">
         Hi! I’m Firdha Fitria, a self-taught makeup artist based in Depok. I’ve loved makeup since 2010 and turned my passion into a profession in 2014. Over the years, I’ve learned from some of the best artists like Anpa Suha, Marlene Hariman, and Laode Yusuf to keep improving my skills.
         What I love most is seeing my clients smile and feel beautiful—it’s the best part of my job. I’ve had the honor of working on exciting projects, from winning 1st place in a national makeup competition to collaborating with brands like Sari Ayu and Safi Indonesia. I was also named one of the TOP 9 Wardah Inspiring MUAs in 2020 and a Top 10 finalist at the Lavie en Awards 2022.
         At Beautyworksbyfifi, I combine passion, creativity, and experience to create timeless looks for your special moments.
