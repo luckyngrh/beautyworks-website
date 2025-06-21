ec7b7d9448face2209b0bfe30917080579fccd10
luckyngrh <nuglucky03@gmail.com>
2025-06-21 11:26:22 +0700

    feat: enhance contact page layout and update contact information links

--- resources/views/hubungi-kami.blade.php
+++ resources/views/hubungi-kami.blade.php
@@ -1,37 +1,30 @@
 <x-layout>
   <div class="container mx-auto px-4 py-10 items-center">
-    <h1 class="text-center text-pink-600 text-3xl font-semibold mb-2">Beautyworks by Fifi</h1>
-    <h2 class="text-center text-pink-600 text-2xl font-medium mb-10">Contact Us</h2>
+    <h1 class="text-center text-5xl font-semibold font-allura mb-2">Beautyworks by Fifi</h1>
+    <h2 class="text-center text-2xl font-medium mb-10">Contact Us</h2>
 
     <div class="grid md:grid-cols-2 gap-8 items-start">
       <!-- Map -->
       <div class="w-full">
-        <iframe class="rounded-xl w-full h-80"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.2983282269775!2d106.8291389!3d-6.223876199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec1f499ea449%3A0x53ff4e511e33d2b5!2sBeautyworks%20by%20Fifi%20Wedding%20Gallery!5e0!3m2!1sen!2sid!4v1718600000000"
-          allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.213076529571!2d106.8339315327303!3d-6.366464926052806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec79474dacd5%3A0x2665334c11531d1c!2sBeautyworks%20by%20Fifi%20Wedding%20Gallery!5e0!3m2!1sen!2sid!4v1750479927521!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
       <!-- Contact Info -->
       <div class="space-y-6">
         <div class="flex items-start space-x-4">
-          <!-- <img src="{{ asset('icons/location.png') }}" class="w-6 h-6 mt-1" alt="Location Icon"> -->
-          <p class="text-pink-600 font-medium">
-            Jl. Margonda No.459, Pondok Cina, Kecamatan Beji,<br>
-            Kota Depok, Jawa Barat 16424
-          </p>
+        <a class="hover:underline" href="https://maps.app.goo.gl/pWUriMmJb7gBfEaL6" target="_blank"><i class="bi bi-geo-alt-fill"></i>
+          Jl. Margonda No.459, Pondok Cina, Kecamatan Beji,<br>
+          Kota Depok, Jawa Barat 16424
+        </a>
         </div>
         <div class="flex items-center space-x-4">
-          <!-- <img src="{{ asset('icons/phone.png') }}" class="w-6 h-6" alt="Phone Icon"> -->
-          <p class="text-pink-600 font-semibold">081917031992</p>
+          <a class="hover:underline" href="https:/wa.me/+6281917031992" target="_blank"><i class="bi bi-whatsapp"></i> 081917031992</a>
         </div>
         <div class="flex items-center space-x-4">
-          <!-- <img src="{{ asset('icons/email.png') }}" class="w-6 h-6" alt="Email Icon"> -->
-          <p class="text-pink-600">Beautyworksbyfifi@gmail.com</p>
+          <a class="hover:underline" href="mailto:beautyworksbyfifi@gmail.com" target="_blank"><i class="bi bi-envelope-heart-fill"></i> Beautyworksbyfifi@gmail.com</a>
         </div>
         <div class="flex items-center space-x-4">
-          <!-- <img src="{{ asset('icons/instagram.png') }}" class="w-6 h-6" alt="Instagram Icon"> -->
-          <p class="text-pink-600">@beautyworksbyfifi</p>
+          <a class="hover:underline" href="https:/www.instagram.com/beautyworksbyfifi" target="_blank"><i class="bi bi-instagram"></i> @beautyworksbyfifi</a>
         </div>
       </div>
     </div>
