05bfc8b7d27c002e5e4a16b7fb3962b53a66f63f
luckyngrh <nuglucky03@gmail.com>
2025-06-22 09:58:41 +0700

    fix: correct form structure for search functionality in kelas makeup page

--- resources/views/dashboard/kelas-makeup.blade.php
+++ resources/views/dashboard/kelas-makeup.blade.php
@@ -2,15 +2,15 @@
   <H1 class="text-4xl mb-3">Daftar Kelas</H1>
   <div class="flex flex-col md:flex-row gap-4 mb-4 items-baseline">
     <a href="{{ route('dashboard.kelas-makeup') }}" class="btn btn-primary">Kelas Makeup</a>
-    
-    <div class="join">
+
+    <form action="{{ route('dashboard.kelas-makeup') }}" method="GET" class="join">
       <div>
         <label class="input validator join-item">
           <input type="text" placeholder="Cari Nama" name="search" value="{{ request('search') }}" />
         </label>
       </div>
-      <button class="btn btn-primary join-item bi bi-search-heart-fill text-accent text-xl"></button>
-    </div>
+      <button type="submit" class="btn btn-primary join-item bi bi-search-heart-fill text-accent text-xl"></button>
+    </form>
   </div>
 
   <div class="overflow-x-auto rounded-box border border-base-content bg-base-200">
