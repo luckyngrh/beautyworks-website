2452a2a330d5ab10a9fe5abbb735a896586ad768
luckyngrh <nuglucky03@gmail.com>
2025-06-23 08:42:34 +0700

    feat: add migration for 'nama' and 'kontak' columns in appointments and reservations tables; create views for appointment and reservation management

---
+++ database/migrations/2025_06_23_005112_add__column_nama_kontak_to_appointments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_user')->nullable()->change();
+            $table->string('nama')->nullable()->after('id_user');
+            $table->string('kontak')->nullable()->after('nama');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            //
+        });
+    }
+};
